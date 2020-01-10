ae8f987a1c8398a48ad9aa31a1b871978d1531c8
Qalac <officialqalac@gmail.com>
2020-01-10 04:22:35 +0000

    altered present field

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ class Visitor(models.Model):
     last_name = models.CharField(max_length=44)
     plate_number = models.CharField(max_length=8, unique=True, primary_key=True, default='0000-000')
     reason = models.TextField(max_length=100, null=True)
-    present = models.BooleanField(default=False)
+    present = models.BooleanField(default=True)
     arrival = models.DateTimeField(auto_now_add = True)
     departure = models.DateTimeField(auto_now=True)
     objects = models.Manager
